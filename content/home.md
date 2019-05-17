7652c7cfc56fa19268c30c6fd65f5c00326eb5d2
Upepo Mwindaji <upepo.mwindaji@gmail.com>
2019-05-18 00:03:57 +0200

    first coommit

---
+++ content/home.md
@@ -0,0 +1,13 @@
+---
+date: 2019-05-09T19:31:20.591Z
+title: nuxt-netlify
+subtitle: welcome
+links:
+  - label: Documentation
+    href: https://www.netlifycms.org/docs
+  - label: Github
+    href: https://github.com/nuxt/nuxt.js"
+---
+Welcome to Netlify CMS demo for Nuxt.
+
+This page is built with Nuxt, and content is managed in Netlify CMS
---
+++ nuxt.config.js
@@ -0,0 +1,77 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    '@nuxtjs/bulma',
+    '@nuxtjs/pwa'
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader'
+      })
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
